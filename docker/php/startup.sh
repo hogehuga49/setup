faddf9e9d4448e9bdb1452333e6683af48e2f619
H.Taiki <t.h.baske49@gmail.com>
2024-03-07 00:07:11 +0900

    Laravelの初期設定をコンテナ立ち上げ時に実行_安定

--- docker/php/startup.sh
+++ docker/php/startup.sh
@@ -2,8 +2,10 @@
 # ==================
 # Laravel初期設定
 # ==================
-cd my-laravel-app/
+cd /var/www/html/my-laravel-app
 composer install
 cp .env.example .env
 php artisan key:generate
 chmod -R 777 storage
+# /bin/sh -c "while sleep 1000; do :; done"
+# sleep infinity
