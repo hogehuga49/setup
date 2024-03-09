92e5284496ef5b6db50f8b78e483892031a8b825
H.Taiki <t.h.baske49@gmail.com>
2024-03-09 11:43:00 +0900

    コンテナのリビルド時に.envが上書きされるので修正

--- docker/php/startup.sh
+++ docker/php/startup.sh
@@ -4,7 +4,8 @@
 # ==================
 cd /var/www/html/my-laravel-app
 composer install
-cp .env.example .env
+# ↓docker-compose.ymlを修正したりコンテナのリビルドが発生すると上書きされるので削除
+# cp .env.example .env
 php artisan key:generate
 chmod -R 777 storage
 # /bin/sh -c "while sleep 1000; do :; done"
